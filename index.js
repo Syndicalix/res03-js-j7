82a803bdb70e204057ec88c370fd8708db5717d8
Syndicalix <sebastienspeich@ip-172-31-22-24.eu-west-3.compute.internal>
2023-01-11 11:57:35 +0000

    hmmm marioooo let's goo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { Pizza } from './pizza.js';
+import {Pizza} from './pizza.js';
 
 window.addEventListener("DOMContentLoaded", function(){
     
-    let pizza = new Pizza("Opel", "Corsa", 5);
+    let pizza = new Pizza("Tomate", "Fromage", "Légumes");
     
 });
--- pizza.js
+++ pizza.js
@@ -1,4 +1,4 @@
-class Pizza {
+/* class Pizza {
     #name;
     #ingredient1;
     #ingredient2;
@@ -44,4 +44,98 @@ class Pizza {
     }
 }
 
+class LaVeggie {
+    #tomate;
+    #fromage;
+    #légumes;
+}
+
+class LaMeattie {
+    #tomate;
+    #fromage;
+    #viande;
+}
+
+class LaFishie {
+    #tomate;
+    #fromage;
+    #poisson;
+}
+
+
+
 export { Pizza };
+
+*/
+
+class Pizza {
+    
+    #name;
+    #ingredient1;
+    #ingredient2;
+    #ingredient3;
+    
+    constructor(name, ingredient1, ingredient2, ingredient3) {
+        this.#name = name;
+        this.#ingredient1 = ingredient1;
+        this.#ingredient2 = ingredient2;
+        this.#ingredient3 = ingredient3;
+    }
+    
+    get name() {
+        return this.#name;
+    }
+    
+    set name (name) {
+        this.#name = name;
+    }
+    
+    get ingredient1() {
+        return this.#ingredient1;
+    }
+    
+    set ingredient1 (ingredient1) {
+        this.#ingredient1 = ingredient1;
+    }
+    
+    get ingredient2() {
+        return this.#ingredient2;
+    }
+    
+    set ingredient2(ingredient2) {
+        this.#ingredient2 = ingredient2;
+    }
+    
+    get ingredient3() {
+        return this.#ingredient3;
+    }
+    
+    set ingredient3 (ingredient3) {
+        this.#ingredient3 = ingredient3;
+    }
+    
+    
+    name()
+    {
+        LaVeggie
+        LaMeattie
+        LaFishie
+    }
+    
+    ingredient1()
+    {
+        tomate
+    }
+    
+    ingredient2()
+    {
+        fromage
+    }
+    
+    ingredient3()
+    {
+        légumes
+        viande
+        poisson
+    }
+}
